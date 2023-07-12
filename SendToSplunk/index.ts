e078daebcc38f2e9406bfa3adc36e58bc2103988
PCOffline <bekerman122@gmail.com>
2023-07-13 01:17:48 +0300

    fix: proper sending of data to splunk

--- SendToSplunk/index.ts
+++ SendToSplunk/index.ts
@@ -1,17 +1,20 @@
-import type { AzureFunction, Context, Timer } from "@azure/functions";
-import { QueueServiceClient } from "@azure/storage-queue";
+import type { AzureFunction, Context } from '@azure/functions';
+import { QueueServiceClient } from '@azure/storage-queue';
 import axios from 'axios';
-import { config } from "../config";
+import { config } from '../config';
 
 const timerTrigger: AzureFunction = async function (
   context: Context,
-  myTimer: Timer
 ): Promise<void> {
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0 as any;
+
+  context.log('✉️ Sending data to Splunk');
+
   const queueServiceClient = QueueServiceClient.fromConnectionString(
-    process.env.CONNECTION_STRING
+    process.env.AzureWebJobsStorage,
   );
   const queueClient = queueServiceClient.getQueueClient(
-    'splunk-send-queue'
+    'splunk-send-queue',
   );
   // 32 is the max number of messages that is possiable to poll
   const maxNumberOfMessages = 32;
@@ -21,24 +24,26 @@ const timerTrigger: AzureFunction = async function (
   while (receivedMsgsResp.receivedMessageItems.length) {
     const uniqueQueueMessages: Set<string> = new Set(
       receivedMsgsResp.receivedMessageItems.map(({ messageText }) =>
-        Buffer.from(messageText, "base64").toString()
-      )
+        Buffer.from(messageText, 'base64').toString(),
+      ),
     );
 
     await Promise.allSettled(
       receivedMsgsResp.receivedMessageItems.map(async (recivedMessage) =>
         queueClient.deleteMessage(
           recivedMessage.messageId,
-          recivedMessage.popReceipt
-        )
-      )
+          recivedMessage.popReceipt,
+        ),
+      ),
     );
 
-    await Promise.allSettled([...uniqueQueueMessages].map((message) => axios.post(config.splunkUrl, message, {
+    await Promise.allSettled([...uniqueQueueMessages].map((message) => axios.post(config.splunkUrl, { event: message }, {
       headers: {
-        authorization: `Splunk ${config.splunkToken}`
-      }
-    })))
+        authorization: `Splunk ${config.splunkToken}`,
+      },
+    })));
+
+    context.log('✔️ Data sent successfully');
 
     receivedMsgsResp = await queueClient.receiveMessages({
       numberOfMessages: maxNumberOfMessages,
